2401c0982d304e9c2bde34ae3271e898ae2d88bf
Boye <akingbelureadeboye18@yahoo.com>
2022-03-15 05:22:58 -0700

    u

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "putchar.h"
+/**
+ * main - Entry point
+ * Description: Program to print putchar and move to next line
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char text[9] = "Holberton";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(text[i]);
+	}
+	
+	_putchar('\n');
+	return (0);
+}

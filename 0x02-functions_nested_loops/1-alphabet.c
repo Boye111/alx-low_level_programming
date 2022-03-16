b2ba21c0c4b6cc135fcaccff5ffa8a0f7d0e099c
Boye <akingbelureadeboye18@yahoo.com>
2022-03-16 07:21:59 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - print alphabet in lowercase
+ * Return: void.
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		++ch;
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main - check the code for Alx 
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}

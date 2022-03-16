324c516a4173f65510b1d966c9f4dc641a628db1
Boye <akingbelureadeboye18@yahoo.com>
2022-03-16 09:16:45 -0700

    o

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,8 @@
  * _isalpha - checks if int is a letter
  * @c: int is to be checked
  *
- * Description: takes an input and checks for letters 
+ * Description: takes an input and checks for letters
+ *
  * Return: 1 if it is a letter 0 if it isn't
  */
 int _isalpha(int c)

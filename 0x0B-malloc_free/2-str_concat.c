51b7fd3a8f522f7888f67e08fc002a120f26fe84
Boye <akingbelureadeboye18@yahoo.com>
2022-04-06 07:11:23 -0700

    error

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,12 +1,10 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
  * str_concat - Concatenates two strings
  * @s1: first string
  * @s2: second string
- *
  * Return: two strings concatenated
  */
 char *str_concat(char *s1, char *s2)
@@ -14,31 +12,21 @@ char *str_concat(char *s1, char *s2)
 	unsigned int len1, len2, size, i, j;
 	char *nstr;
 
-
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
-
-
 	len1 = 0;
 	while (s1[len1] != '\0')
 		len1++;
-
 	len2 = 0;
 	while (s2[len2] != '\0')
 		len2++;
-
 	size = len1 + len2;
-
 	nstr = malloc((sizeof(char) * size) + 1);
-
 	if (nstr == NULL)
 		return (NULL);
-
 	i = 0;
-
 	while (i < len1)
 	{
 		nstr[i] = s1[i];

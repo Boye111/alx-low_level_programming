f13c16401671fa51f828e1f6a7eea65507ed4f6d
Boye <akingbelureadeboye18@yahoo.com>
2022-04-07 11:33:07 -0700

    NEED IT

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	l = nmemb * size;
-	p = malloc(1);
+	p = malloc(l);
 	if (p == NULL)
 		return (NULL);
 	while (i < l)

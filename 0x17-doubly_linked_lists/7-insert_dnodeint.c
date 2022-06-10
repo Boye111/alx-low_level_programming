2d277ffc809f3a6f6081ed71770d6a1f38a5d7da
Boye <adeboyeakingbelure18@yahoo.com>
2022-06-10 07:01:28 -0700

    op

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (h == NULL)
 		return (NULL);
-	if (*h == NULL && idx == 0);
+	if (*h == NULL && idx == 0)
 	return (add_dnodeint(h, n));
 	length = dlistint_len(*h);
 	if (idx == 0)
